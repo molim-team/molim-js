cf0acc83761681b86a283207ef90d70c17a65a16
molim-team <YOUR_MOLIM_EMAIL>
2026-06-10 14:05:24 +0300

    update next

--- next.config.js
+++ next.config.js
@@ -7,6 +7,19 @@ const nextConfig = {
       'puppeteer-core'
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
